be0eab5952d100b979a5ef6ebda948902e007cf3
김승민 <tmdals6102@gmail.com>
2025-03-31 16:08:14 +0900

    TextView의 속성: Button & EditText

--- app/src/main/java/kr/ac/kopo/basicwidget/MainActivity.java
+++ app/src/main/java/kr/ac/kopo/basicwidget/MainActivity.java
@@ -2,6 +2,9 @@ package kr.ac.kopo.basicwidget;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -12,24 +15,44 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText edit1;
+    TextView text1;
+    TextView text2;
+    TextView text3;
+    TextView textResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+            setContentView(R.layout.activity_main2);
 
-        TextView text1 = findViewById(R.id.text1);
-        TextView text2 = findViewById(R.id.text2);
-        TextView text3 = findViewById(R.id.text3);
+            text1 = findViewById(R.id.text1);
+            text2 = findViewById(R.id.text2);
+            text3 = findViewById(R.id.text3);
 
-        text1.setTextSize(20);
-        text1.setTextColor(Color.MAGENTA);
-        text1.setText("KoreaPolyTechCollege");
+            text1.setTextSize(20);
+            text1.setTextColor(Color.MAGENTA);
+            text1.setText("KoreaPolyTechCollege");
 
-        text2.setTextSize(30);
-        text2.setTextColor(Color.RED);
-        text2.setText("인공지능소프트웨어과");
+            text2.setTextSize(30);
+            text2.setTextColor(Color.RED);
+            text2.setText("인공지능소프트웨어과");
 
-        text3.setText("SinglgLine속성SinglgLine속성SinglgLine속성SinglgLine속성SinglgLine속성SinglgLine속성SinglgLine속성SinglgLine속성");
-        text3.setSingleLine();
-    }
+            text3.setText("SinglgLine속성SinglgLine속성SinglgLine속성SinglgLine속성SinglgLine속성SinglgLine속성SinglgLine속성SinglgLine속성");
+            text3.setSingleLine(true);
+
+            edit1 = findViewById(R.id.edit1);
+            textResult = findViewById(R.id.textResult);
+            Button btnResult = findViewById(R.id.btnResult);
+
+            btnResult.setOnClickListener(btnListener);
+        }
+        View.OnClickListener btnListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String univName = edit1.getText().toString();
+                String result = univName + "에 합격하신 것을 진심으로 축하드립니다.";
+                 textResult.setText(result);
+                 textResult.setTextColor(Color.rgb(92,209,229));
+            }
+        };
 }
